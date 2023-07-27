5729eb0d5e37918ea7990f918b07adc253a4fa01
Abdijibar123 <abdijibar1313@gmail.com>
2023-07-27 16:58:24 +0300

    final joke app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import TwoPart from "./TwoPart";
 import SingleJoke from "./SingleJoke";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [joke,setJoke] = useState({})
+
+  function getJoke(){
+    fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=religious")
+    .then((res)=>{
+      return res.json();
+    })
+    .then((data)=>{
+      setJoke(data)
+      console.log(data)
+    })
+  }
+
+useEffect(()=>{
+  getJoke();
+} ,[]);
+
   return (
     <div className="app">
       <h2 className="heading">😂 Jokes! 😂</h2>
-      <SingleJoke />
-      <TwoPart />
+      {joke.type === "single" ? <SingleJoke data={joke} /> : <TwoPart data={joke}/>}
+      
+      
 
       <div className="buttons">
-        <button className="another">Another Joke!😆</button>
+        <button className="another" onClick={getJoke}>Another Joke!😆</button>
       </div>
     </div>
   );
